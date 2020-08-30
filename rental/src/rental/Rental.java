3a632367a5c876fb45215ef494288f67e5e51322
jotii666 <jotii666@users.noreply.github.com>
2020-08-30 21:17:27 +0700

    Jotii666

---
+++ rental/src/rental/Rental.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rental;
+import rental.Jrental;
+/**
+ *
+ * @author A S U S
+ */
+public class Rental {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Jrental().setVisible(true);
+    }
+    
+}
